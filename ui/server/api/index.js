da4cfd2943e06d2f92a085491c2c94ea3bf5664b
redhood <anishksaha1997@gmail.com>
2021-02-24 01:09:56 +0530

    gateway

--- ui/server/api/index.js
+++ ui/server/api/index.js
@@ -1,3 +1,26 @@
+const express = require('express');
+const httpProxy = require('express-http-proxy');
+const app = express();
 
+const odinServiceProxy = httpProxy('https://odin.kaiyo.tech');
 
-//  odin ke api
+app.use((req, res, next) => {
+    /*
+        Authentication login -> preferably use a JWT token with no persistence
+        required. Just use a timestamp for the token-liveliness
+
+        Also, pack some user-claims data which will be required by the ACL module
+        for role-based resource filtering
+
+    */
+    next()
+})
+
+// Main a list of odin endpoints
+
+var odinGetDeploymentEndpoint = '/getDeployment/{deploymentId}'
+
+app.get('/getDeployment/{deploymentId}', (req, res, next) => {
+    odinServiceProxy(req, res, next)
+})
+
