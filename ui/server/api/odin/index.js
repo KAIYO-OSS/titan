707d7bdedfa0a2446c5763b12ba0b97beddbfb4f
redhood <anishksaha1997@gmail.com>
2021-03-19 00:53:54 +0530

    initial commit

--- ui/server/api/odin/index.js
+++ ui/server/api/odin/index.js
@@ -4,8 +4,15 @@ const app = express.Router();
 // Main a list of odin endpoints
 
 var odinGetDeploymentEndpoint = '/getDeployment/{deploymentId}'
+app.get('/health', (req, res, next) => {
+    res.send({'Hello': 'World'});
+});
 
+app.get('/docs', (req, res, next) => {
+    fetch('http://localhost:5000/docs').then(resp => {res.send(resp.body)});
+});
 // app.get('/getDeployment/{deploymentId}', (req, res, next) => {
 //
 // })
 module.exports = app;
+// Nahin likhey toh milta nahin logon ko
