3d7f53111a6b470ee1cd006bb3460a13566539b3
redhood <anishksaha1997@gmail.com>
2021-03-20 22:08:22 +0530

    Odin apis added

--- ui/server/api/odin/index.js
+++ ui/server/api/odin/index.js
@@ -3,7 +3,69 @@ const app = express.Router();
 
 const odinBaseUrl = 'http://localhost:8083/api';
 
-/* details apis */
+/* Odin APIs */
+const deployWorkspace = '/odin/deploy/workspace/';
+const deleteWorkspace = '/odin/remove/workspace/'; // workspaceId
+const deployService = '/odin/deploy/service/';
+const updateDeployService = '/odin/update/service/';
+const deleteDeployment = '/odin/remove/service/'; // deploymentId  
+
+app.post('/odin/deploy/workspace/', (req, res, next) => {
+    console.log('Deploy workspace called with req =>', req.body);
+    fetch(odinBaseUrl + deployWorkspace)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+app.delete('/odin/remove/workspace/:workspaceId', (req, res, next) => {
+    console.log('Delete workspace called with workspaceId => ', req.params.workspaceId);
+    fetch(odinBaseUrl + deleteDeployment + req.params.workspaceId)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+app.post('/odin/deploy/service/', (req, res, next) => {
+    console.log('Deploy workspace called with req => ', req.body);
+    fetch(odinBaseUrl + deployService)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+app.post('/odin/update/service/', (req, res, next) => {
+    console.log('Update service called with req => ' , req.body);
+    fetch(odinBaseUrl + updateDeployService)
+    .then(odinResp => {
+        return odinResp.json()
+    })
+    .then(aboveResp => {
+        res.send(aboveResp.body)
+    })
+});
+
+app.delete('/odin/remove/service/:deploymentId', (req, res, next) => {
+    console.log('Delete deployment called with req => ', req.params.deploymentId);
+    fetch(odinBaseUrl + deleteDeployment + req.params.deploymentId)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+/* Details APIs */
 const workspaceInformation = '/details/workspace/info/'; 
 const allUserWorkspaces = '/details/workspaces/'; 
 const allUserServicesInWorkspace = '/details/services/'; 
@@ -17,72 +79,130 @@ app.get('/health', (req, res, next) => {
 });
 
 app.get('/details/workspace/info/:workspaceId', (req, res, next) => {
+    console.log('Workspace Information called for workspaceId => ' + req.params.workspaceId);
     fetch(odinBaseUrl + workspaceInformation + req.params.workspaceId)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResonse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
 app.get('/details/workspace/:userId', (req, res, next) => {
+    console.log('All User workspaces called for usedId => ' + req.params.userId);
     fetch(odinBaseUrl + allUserWorkspaces + req.params.userId)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResponse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
 app.get('/details/services/:workspaceId', (req, res, next) => {
+    console.log('All user services in workspaces called for workspaceId => ' + req.params.workspaceId);
     fetch(odinBaseUrl + allUserServicesInWorkspace + req.params.workspaceId)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResponse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
 app.get('/details/deployments/:userId', (req, res, next) => {
+    console.log('All user deployments called for userId => ' + req.params.userId);
     fetch(odinBaseUrl + allUserDeployment + req.params.userId)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResponse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
 app.get('/details/service/info/:deploymentId', (req, res, next) => {
+    console.log('Service information called for deploymentId => ' + req.params.deploymentId);
     fetch(odinBaseUrl + serviceInformation + req.params.deploymentId)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResponse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
 app.get('/details/service/configuration/:deploymentId', (req, res, next) => {
+    console.log('Current configuration called for deploymentId => ' + req.params.deploymentId);
     fetch(odinBaseUrl + currentConfiguration + req.params.deploymentId)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResponse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
 app.get('/details/service/configurationall', (req, res, next) => {
+    console.log('All configurations called');
     fetch(odinBaseUrl + currentConfiguration)
-        .then(odinResponse => {
-            return odinResponse.json()
+        .then(odinResp => {
+            return odinResp.json()
         })
-        .then(aboveResponse => {
-            res.send(aboveResponse.body)
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+/* Polling APIs */
+const pollingCreateWorkspace = '/polling/deploy/workspace/'; //workspaceId
+const pollingDeleteWorkspace = '/polling/remove/workspace/'; //workspaceId
+const pollingDeployService = '/polling/deploy/service/'; //deploymentId
+const pollingUpdateService = '/polling/update/service/'; //deploymentId
+const pollingDeleteService = '/polling/delete/service/'; //deployemtId
+
+app.get('/polling/deploy/workspace/:workspaceId', (req, res, next) => {
+    console.log('Polling create workspace called for workspaceId => ' + req.params.workspaceId);
+    fetch(odinBaseUrl + pollingCreateWorkspace + req.params.workspaceId)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+app.get('/polling/remove/workspace/:workspaceId', (req, res, next) => {
+    console.log('Polling delete workspace called for workspaceId =>' + req.params.workspaceId);
+    fetch(odinBaseUrl + pollingDeleteWorkspace + req.params.workspaceId)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+app.get('/polling/deploy/service/:deploymentId', (req, res, next) => {
+    console.log('Polling deploy service called for deploymentId => ' + req.params.deploymentId);
+    fetch(odinBaseUrl + pollingDeployService + req.params.deploymentId)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
+        });
+});
+
+app.get('/polling/update/service/:deploymentId', (req, res, next) => {
+    console.log('Polling update service called for deploymentId =>' + req.params.workspaceId);
+    fetch(odinBaseUrl + pollingUpdateService + req.params.workspaceId)
+        .then(odinResp => {
+            return odinResp.json()
+        })
+        .then(aboveResp => {
+            res.send(aboveResp.body)
         });
 });
 
