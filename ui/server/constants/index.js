42923e94869bc9a898094e6a7cb6a6925b2dd445
anuragsarkar97 <sarkar.anurag@outlook.com>
2021-03-20 12:55:09 +0530

    added db link and index js

--- ui/server/constants/index.js
+++ ui/server/constants/index.js
@@ -1,3 +1,4 @@
 export const JWT_SECRET = process.env.JWT_SECRET || "";
 export const SERVICE_NAME = process.env.SERVICE_NAME || "titan-ui-middleware";
 export const PORT = process.env.PORT || 8083;
+export const DB_URL = process.env.ETCD3_URL || "127.0.0.1:2379";
--- ui/server/db/index.js
+++ ui/server/db/index.js
@@ -1,19 +1,33 @@
+import {DB_URL} from "../constants";
+
 const {Etcd3} = require('etcd3');
-const client = new Etcd3();
+const client = new Etcd3({hosts: DB_URL});
 
 
-export function get(key) {
-
+export async function get(key) {
+    let data;
+    data = await client.get(key)
+    return data;
 }
 
-export function put(key, value) {
-
+export async function put(key, value) {
+    let data;
+    data = await client
+        .put(key)
+        .value(value)
+    console.log(`put executed with key := ${key} and value := ${value}`)
+    return data
 }
 
-export function find(key) {
-
+export async function prefixFind(key) {
+    let data;
+    data = await client.getAll().prefix(key).keys();
+    return data;
 }
 
-export function deleteData(key) {
-
+export async function del(key) {
+    let data;
+    data = await client.delete().key(key);
+    console.log(`delete executed with key := ${key}`)
+    return data;
 }
