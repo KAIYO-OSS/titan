56bff91d6e409f0fae6c54b4717959228e2c966a
Dhiraj Kumar Jain <jaindhir@amazon.com>
2021-05-01 23:00:03 +0530

    Remove Azure and MongoDb from code

--- api/routers/details.py
+++ api/routers/details.py
@@ -1,53 +1,59 @@
-from fastapi import APIRouter
-from util.utilityHelpers import Utils
-import util.mongoUtil as mu
-router = APIRouter()
-
-
-@router.get("/details/workspace/info/{workspaceId}", tags=["details"])
-async def worksapce_information(workspaceId: str):
-    try:
-        return mu.find_by_id("workspace", workspaceId)
-    except Exception as ex:
-        return ex
-
-@router.get("/details/workspaces/{userId}", tags=["details"])
-async def all_user_workspaces(userId: str):
-    try:
-        return mu.find_by_userId("workspace", userId)
-    except Exception as ex:
-        return ex
-
-@router.get("/details/services/{workspaceId}", tags=["details"])
-async def all_user_services_in_workspace(userId: str):
-    try:
-        return mu.find_by_workspace("service", workspaceId)
-    except Exception as ex:
-        return ex
-
-@router.get("/details/deployments/{userId}", tags=["details"])
-async def all_user_deployment(userId: str):
-    try:
-        return mu.find_by_userId("service", userId)
-    except Exception as ex:
-        return ex
-
-@router.get("/details/service/info/{deploymentId}", tags=["details"])
-async def service_information(deploymentId: str):
-    try:
-        return mu.find_by_id("service", deploymentId)
-    except Exception as ex:
-        return ex
-
-@router.get("/details/service/configuration/{deploymentId}", tags=["details"])
-async def current_configuration(deploymentId: str):
-    try:
-        service = mu.find_by_id("service", deploymentId)
-        return Utils.getJson(service["values"])
-    except Exception as ex:
-        return ex
-
-
-@router.get("/details/service/configurationall", tags=["details"])
-async def all_configuration():
-    return {"message": "Hello World"}
+# from fastapi import APIRouter
+# from util.utilityHelpers import Utils
+# import util.mongoUtil as mu
+#
+# router = APIRouter()
+#
+#
+# @router.get("/details/workspace/info/{workspaceId}", tags=["details"])
+# async def worksapce_information(workspaceId: str):
+#     try:
+#         return mu.find_by_id("workspace", workspaceId)
+#     except Exception as ex:
+#         return ex
+#
+#
+# @router.get("/details/workspaces/{userId}", tags=["details"])
+# async def all_user_workspaces(userId: str):
+#     try:
+#         return mu.find_by_userId("workspace", userId)
+#     except Exception as ex:
+#         return ex
+#
+#
+# @router.get("/details/services/{workspaceId}", tags=["details"])
+# async def all_user_services_in_workspace(userId: str):
+#     try:
+#         return mu.find_by_workspace("service", workspaceId)
+#     except Exception as ex:
+#         return ex
+#
+#
+# @router.get("/details/deployments/{userId}", tags=["details"])
+# async def all_user_deployment(userId: str):
+#     try:
+#         return mu.find_by_userId("service", userId)
+#     except Exception as ex:
+#         return ex
+#
+#
+# @router.get("/details/service/info/{deploymentId}", tags=["details"])
+# async def service_information(deploymentId: str):
+#     try:
+#         return mu.find_by_id("service", deploymentId)
+#     except Exception as ex:
+#         return ex
+#
+#
+# @router.get("/details/service/configuration/{deploymentId}", tags=["details"])
+# async def current_configuration(deploymentId: str):
+#     try:
+#         service = mu.find_by_id("service", deploymentId)
+#         return Utils.getJson(service["values"])
+#     except Exception as ex:
+#         return ex
+#
+#
+# @router.get("/details/service/configurationall", tags=["details"])
+# async def all_configuration():
+#     return {"message": "Hello World"}
--- api/routers/odin.py
+++ api/routers/odin.py
@@ -1,68 +1,26 @@
 from fastapi import APIRouter
-from util.utilityHelpers import Utils
+from util.helm import Helm
 from models.deployRequest import DeployRequest
-from models.workspaceRequest import WorkspaceRequest
-from models.updateRequest import UpdateRequest
-from service.plugins.azure.azure_aks_deploy import OdinUserServiceDeployment, OdinUserServiceDelete, OdinUserWorkspaceDeployment,OdinUserServiceUpgrade, OdinUserWorkspaceDelete, OdinWorkspaceHealth
-import util.mongoUtil as mu
+
 router = APIRouter()
 
-
-@router.post("/odin/deploy/workspace/", tags=["odin"])
-async def deploy_workspace(workspaceRequest: WorkspaceRequest):
-    try:
-        Utils.isValidProvider(workspaceRequest.provider)
-        deploy = OdinUserWorkspaceDeployment(workspaceRequest)
-        if str.lower(workspaceRequest.provider) == "azure":
-            deploy.deployAzureWorkspace()
-            return {
-                "status": "200",
-                "metadata": {},
-                "error": ""
-            }
-    except Exception as ex:
-        return {
-            "status": "500",
-            "metadata": {},
-            "error": "Workspace deployment failed: " + str(ex)
-        }
-
-@router.delete("/odin/remove/workspace/{workspaceId}", tags=["odin"])
-async def delete_workspace(workspaceId: str):
-    try:
-        workspace = mu.find_by_id("workspace", workspaceId)
-        clusterName = workspace["clusterName"]
-        rgName = workspace["resourceGroupName"]
-        odinServiceDelte = OdinUserWorkspaceDelete(workspaceId, rgName, clusterName)
-        odinServiceDelte.deleteAzureWorkspace()
-        return {
-            "status": "200",
-            "metadata": {},
-            "error": ""
-        }
-    except Exception as ex:
-        return {
-            "status": "500",
-            "metadata": {},
-            "error": "Workspace delete failed: " + str(ex)
-        }
 
 @router.post("/odin/deploy/service/", tags=["odin"])
 async def deploy_service(deploy_request: DeployRequest):
     try:
-        workspace = mu.find_by_id("workspace", deploy_request.workspaceID)
-        rg = workspace["resourceGroupName"]
-        cluster = workspace["clusterName"]
-        deploy = OdinUserServiceDeployment(deploy_request.releaseName, deploy_request.env,
-                                        Utils.getService(deploy_request.serviceId), deploy_request.values, rg,
-                                        cluster,deploy_request.userId,deploy_request.workspaceID,deploy_request.serviceId)
-        url, ip = deploy.deployAzureUserService()
+        # workspace = mu.find_by_id("workspace", deploy_request.workspaceID)
+        # rg = workspace["resourceGroupName"]
+        # cluster = workspace["clusterName"]
+        # deploy = OdinUserServiceDeployment(deploy_request.releaseName, deploy_request.env,
+        #                                    Utils.getService(deploy_request.serviceId), deploy_request.values, rg,
+        #                                    cluster, deploy_request.userId, deploy_request.workspaceID,
+        #                                    deploy_request.serviceId)
+        # url, ip = deploy.deployAzureUserService()
+        Helm.odinHelmSetup()
+        output = Helm.deployService(deploy_request.service_name, deploy_request.chart_name, deploy_request.values)
         return {
             "status": "200",
-            "metadata": {
-                "url": url,
-                "ip": ip
-            },
+            "metadata": output,
             "error": ""
         }
     except Exception as ex:
@@ -73,56 +31,66 @@ async def deploy_service(deploy_request: DeployRequest):
         }
 
 
-@router.post("/odin/update/service/", tags=["odin"])
-async def update_deploy_service(update_request: UpdateRequest):
+# @router.post("/odin/update/service/", tags=["odin"])
+# async def update_deploy_service(update_request: UpdateRequest):
+#     try:
+#         service = mu.find_by_id("service", update_request.deploymentId)
+#         workspace = mu.find_by_id("workspace", service["workspaceId"])
+#         rgName = workspace["resourceGroupName"]
+#         clusterName = workspace["clusterName"]
+#         serviceName = service["serviceName"]
+#         odinServiceDelete = OdinUserServiceUpgrade(rgName, clusterName, serviceName, service["_id"],
+#                                                    Utils.getService(service["serviceId"]), update_request.values)
+#         odinServiceDelete.updateAzureUserService()
+#         return {
+#             "status": "200",
+#             "metadata": {},
+#             "error": ""
+#         }
+#     except Exception as ex:
+#         return {
+#             "status": "500",
+#             "metadata": {},
+#             "error": "Service update failed: " + str(ex)
+#         }
+
+
+# @router.delete("/odin/remove/service/{deploymentId}", tags=["odin"])
+# async def delete_deployment(deploymentId: str):
+#     try:
+#         service = mu.find_by_id("service", deploymentId)
+#         workspace = mu.find_by_id("workspace", service["workspaceId"])
+#         rgName = workspace["resourceGroupName"]
+#         clusterName = workspace["clusterName"]
+#         serviceName = service["serviceName"]
+#         odinServiceDelte = OdinUserServiceDelete(rgName, clusterName, serviceName, deploymentId)
+#         odinServiceDelte.deleteAzureUserService()
+#         return {
+#             "status": "200",
+#             "metadata": {},
+#             "error": ""
+#         }
+#     except Exception as ex:
+#         return {
+#             "status": "500",
+#             "metadata": {},
+#             "error": "Service delete failed: " + str(ex)
+#         }
+
+
+@router.get("/odin/monitor/service/{service_name}", tags=["odin"])
+async def get_status(service_name):
     try:
-        service = mu.find_by_id("service", update_request.deploymentId)
-        workspace = mu.find_by_id("workspace", service["workspaceId"])
-        rgName = workspace["resourceGroupName"]
-        clusterName = workspace["clusterName"]
-        serviceName = service["serviceName"]
-        odinServiceDelete = OdinUserServiceUpgrade(rgName, clusterName, serviceName, service["_id"] , Utils.getService(service["serviceId"]), update_request.values)
-        odinServiceDelete.updateAzureUserService()
+        status = Helm.getServiceStatus(service_name)
         return {
             "status": "200",
-            "metadata": {},
+            "metadata": status,
             "error": ""
         }
     except Exception as ex:
         return {
             "status": "500",
             "metadata": {},
-            "error": "Service update failed: " + str(ex)
+            "error": "Failed to fetch Service Status: " + str(ex)
         }
 
-
-@router.delete("/odin/remove/service/{deploymentId}", tags=["odin"])
-async def delete_deployment(deploymentId: str):
-    try:
-        service = mu.find_by_id("service", deploymentId)
-        workspace = mu.find_by_id("workspace", service["workspaceId"])
-        rgName = workspace["resourceGroupName"]
-        clusterName = workspace["clusterName"]
-        serviceName = service["serviceName"]
-        odinServiceDelte = OdinUserServiceDelete(rgName, clusterName, serviceName, deploymentId)
-        odinServiceDelte.deleteAzureUserService()
-        return {
-            "status": "200",
-            "metadata": {},
-            "error": ""
-        }
-    except Exception as ex:
-        return {
-            "status": "500",
-            "metadata": {},
-            "error": "Service delete failed: " + str(ex)
-        }
-
-
-@router.get("/odin/monitor/workspace/{workspaceId}", tags=["odin"])
-async def get_health(workspaceId):
-    workspace = mu.find_by_id("workspace", workspaceId)
-    rgName = workspace["resourceGroupName"]
-    clusterName = workspace["clusterName"]
-    odinWorkspaceHealth = OdinWorkspaceHealth(rgName, clusterName)
-    return odinWorkspaceHealth.getAzureWorkspaceHealth()
