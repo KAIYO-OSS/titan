16f8c8e186637e46dbd3d4a1a2fa303eea53458e
redhood <anishksaha1997@gmail.com>
2021-05-03 02:40:19 +0530

    loads of changes

--- ui/server/api/index.js
+++ ui/server/api/index.js
@@ -4,6 +4,7 @@ const odinApi = require("./odin");
 const acl = require("./../acl");
 const logger = require('./../logger');
 const jwt = require('jsonwebtoken');
+const { response } = require('express');
 
 app
     .use(express.json())
@@ -21,107 +22,36 @@ app
             headers: req.headers,
         });
 
-        var accessToken = req.headers['x-access-token'];
-        console.log('The accessToken passed -> ', accessToken);
-        var claims = acl.decodeTokenForUserInfo(accessToken);
-
-        console.log('The claims acquired from the x-access-token passed =>');
-        console.log(claims);
+        let accessToken = req.headers['x-access-token'];
+        let claims = acl.decodeTokenForUserInfo(accessToken);
 
         if(typeof claims === 'string') {
             claims = JSON.parse(claims);
         }
 
         if(Object.keys(claims).length === 0 && claims.constructor === Object) {
-            return {
-                'statusCode': 401,
-                'data': 'Incorrect user credentials.'
-            };
+            res.status(403);
+            res.send({
+                'msg': 'Incorrect user credentials. Access Denied.'
+            });
         }
 
-        authResp = await acl.authenticateTheUser(claims);
+        let authResp = await acl.authenticateTheUser(claims);
 
         logger.info('The authResp inside API Index => %d', authResp);
 
         if(authResp['status'] === 500) {
+            res.status(500);
             res.send({
-                'statusCode': 500,
-                'data': 'Something went wrong. Contact Tech Support.'
+                'msg': 'Something went wrong. Contact Tech Support.'
             })
         }else if(authResp['status'] === 401) {
+            res.status(401);
             res.send({
-                'statusCode': 401,
-                'data': 'Incorrect user credentials.'
+                'msg': 'Incorrect user credentials.'
             })
         }        
         next();
-    })
-    .user("/login", async function(req, res, next) {
-        var email = req.body['emailId'];
-        var acl = req.body['acl'];
-        var aclSearchKey = "user:email_address-".concat(email);
-        var aclTokenInDB; 
-
-        try {
-            aclTokenInDB = await odinApi.get(aclSearchKey);
-            console.log("The aclTokenInDB -> %s", aclTokenInDB);
-            if(typeof aclTokenInDB == 'undefined') {
-                logger.info("No user found during login for aclSearchKey -> %s", aclSearchKey);
-                res.send({
-                    'status': 403,
-                    'msg': 'Access denied'
-                })
-            }
-            if(acl != aclTokenInDB) {
-                logger.info("Wrong acl token passed with aclSearchKey -> %s", aclSearchKey);
-                res.send({
-                    'status': 403,
-                    'msg': 'Access denied'
-                })
-            }
-        }catch(e) {
-            logger.info("DB error during login", e);
-            res.send({'status': 500, 'msg': 'Internal Server Error'});
-        }
-        /* 
-            Otherwise, the acl-key passed is matched with 
-            the one stored in the DB, so, the user will be
-            given access. Now, try to fetch the claims
-            associated with the user so some data fields
-            like 'role' and 'isActive' which are used
-            frequently can be read with DB transactions.
-        */
-
-        var userInfoSearchKey = "acl:acl_token-".concat(aclTokenInDB);
-        var userInfoForClaims;
-
-        try {
-            userInfoForClaims = await odinApi.get(userInfoSearchKey);
-            if(typeof userInfoForClaims == 'undefined') {
-                logger.info("user data not found for userInfoSearchKey -> %s", userInfoSearchKey);
-                res.send({'status': 404, 'msg': 'User not found'});
-                /*
-                    The status to be returned in this case can
-                    be discussed later but right now 'not found'
-                    seems just appropriate
-                */
-            }
-        }catch(e) {
-            logger.info("DB error encountered in login while fetching user data", e);
-            res.send({'status': 500, 'msg': 'Internal Server Error'});
-        }
-
-        var claimsToBeEncoded = {
-            'email': email,
-            'acl': acl,
-            'isActive': userInfoForClaims['isActive'],
-            'role': userInfoForClaims['role']
-        };
-
-        /* 
-            Rest to be written
-        */
-        
     })
     .use("/", odinApi)
 
--- ui/server/index.js
+++ ui/server/index.js
@@ -8,7 +8,7 @@ const logger = require("./logger");
 const api = require("./api");
 const jwt = require("jsonwebtoken");
 const jwtSecret = "VmYq3t6v9y$B&E)H@McQfTjWnZr4u7x!";
-const create = require("./create");
+const users = require("./users");
 
 const app = express();
 
@@ -23,7 +23,7 @@ var server = app
     }, jwtSecret, {algorithm: 'HS256'})
     ))
     .use("/api", api)
-    .use("/create", create)
+    .use("/users", users)
     .use(cors())
     .use(express.static(path.join(__dirname, "..", "build")))
     .use(bodyParser.json())
@@ -32,4 +32,4 @@ var server = app
     })
     .listen(8085, () => console.log(`Server started http://localhost:8085`));
 
-server.setTimeout(2000);
+//server.setTimeout(2000);
