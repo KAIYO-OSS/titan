5c986ad0463d2ed696239eabce1571d1d71bc229
Dhiraj Kumar Jain <jaindhir@amazon.com>
2021-05-02 16:12:51 +0530

    Add API for Deploy/List/Delete service

--- api/routers/odin.py
+++ api/routers/odin.py
@@ -5,17 +5,9 @@ from models.deployRequest import DeployRequest
 router = APIRouter()
 
 
-@router.post("/odin/deploy/service/", tags=["odin"])
+@router.post("/odin/service/deploy", tags=["odin"])
 async def deploy_service(deploy_request: DeployRequest):
     try:
-        # workspace = mu.find_by_id("workspace", deploy_request.workspaceID)
-        # rg = workspace["resourceGroupName"]
-        # cluster = workspace["clusterName"]
-        # deploy = OdinUserServiceDeployment(deploy_request.releaseName, deploy_request.env,
-        #                                    Utils.getService(deploy_request.serviceId), deploy_request.values, rg,
-        #                                    cluster, deploy_request.userId, deploy_request.workspaceID,
-        #                                    deploy_request.serviceId)
-        # url, ip = deploy.deployAzureUserService()
         Helm.odinHelmSetup()
         output = Helm.deployService(deploy_request.service_name, deploy_request.chart_name, deploy_request.values)
         return {
@@ -55,27 +47,21 @@ async def deploy_service(deploy_request: DeployRequest):
 #         }
 
 
-# @router.delete("/odin/remove/service/{deploymentId}", tags=["odin"])
-# async def delete_deployment(deploymentId: str):
-#     try:
-#         service = mu.find_by_id("service", deploymentId)
-#         workspace = mu.find_by_id("workspace", service["workspaceId"])
-#         rgName = workspace["resourceGroupName"]
-#         clusterName = workspace["clusterName"]
-#         serviceName = service["serviceName"]
-#         odinServiceDelte = OdinUserServiceDelete(rgName, clusterName, serviceName, deploymentId)
-#         odinServiceDelte.deleteAzureUserService()
-#         return {
-#             "status": "200",
-#             "metadata": {},
-#             "error": ""
-#         }
-#     except Exception as ex:
-#         return {
-#             "status": "500",
-#             "metadata": {},
-#             "error": "Service delete failed: " + str(ex)
-#         }
+@router.delete("/odin/service/delete/{service_name}", tags=["odin"])
+async def delete_service(service_name):
+    try:
+        Helm.deleteService(service_name)
+        return {
+            "status": "200",
+            "metadata": {},
+            "error": ""
+        }
+    except Exception as ex:
+        return {
+            "status": "500",
+            "metadata": {},
+            "error": "Service delete failed: " + str(ex)
+        }
 
 
 @router.get("/odin/monitor/service/{service_name}", tags=["odin"])
@@ -92,5 +78,22 @@ async def get_status(service_name):
             "status": "500",
             "metadata": {},
             "error": "Failed to fetch Service Status: " + str(ex)
+        }
+
+
+@router.get("/odin/services/", tags=["odin"])
+async def get_all_services():
+    try:
+        service_list = Helm.listAllServices()
+        return {
+            "status": "200",
+            "metadata": service_list,
+            "error": ""
+        }
+    except Exception as ex:
+        return {
+            "status": "500",
+            "metadata": {},
+            "error": "Failed to fetch all services: " + str(ex)
         }
 
