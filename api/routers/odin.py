75100a4cfcef21b450895c2533c85214bc451569
Dhiraj Kumar Jain <jaindhir@amazon.com>
2021-05-21 01:22:30 +0530

    refactor API routes and prettify json response

--- api/routers/odin.py
+++ api/routers/odin.py
@@ -1,15 +1,19 @@
+import json
+
 from fastapi import APIRouter
 from util.helm import Helm
 from models.deployRequest import DeployRequest
+from util.utilityHelpers import Utils
 
 router = APIRouter()
 
 
-@router.post("/odin/service/deploy", tags=["odin"])
+@router.post("/odin/service/", tags=["odin"])
 async def deploy_service(deploy_request: DeployRequest):
     try:
         Helm.odinHelmSetup()
-        output = Helm.deployService(deploy_request.service_name, deploy_request.chart_name, deploy_request.values)
+        output = Utils.getJson(Helm.deployService(deploy_request.service_name,
+                                                   deploy_request.chart_name, deploy_request.values))
         return {
             "status": "200",
             "metadata": output,
@@ -18,42 +22,17 @@ async def deploy_service(deploy_request: DeployRequest):
     except Exception as ex:
         return {
             "status": "500",
-            "metadata": {},
             "error": "Service deployment failed: " + str(ex)
         }
 
 
-# @router.post("/odin/update/service/", tags=["odin"])
-# async def update_deploy_service(update_request: UpdateRequest):
-#     try:
-#         service = mu.find_by_id("service", update_request.deploymentId)
-#         workspace = mu.find_by_id("workspace", service["workspaceId"])
-#         rgName = workspace["resourceGroupName"]
-#         clusterName = workspace["clusterName"]
-#         serviceName = service["serviceName"]
-#         odinServiceDelete = OdinUserServiceUpgrade(rgName, clusterName, serviceName, service["_id"],
-#                                                    Utils.getService(service["serviceId"]), update_request.values)
-#         odinServiceDelete.updateAzureUserService()
-#         return {
-#             "status": "200",
-#             "metadata": {},
-#             "error": ""
-#         }
-#     except Exception as ex:
-#         return {
-#             "status": "500",
-#             "metadata": {},
-#             "error": "Service update failed: " + str(ex)
-#         }
-
-
-@router.delete("/odin/service/delete/{service_name}", tags=["odin"])
+@router.delete("/odin/service/{service_name}", tags=["odin"])
 async def delete_service(service_name):
     try:
         Helm.deleteService(service_name)
         return {
             "status": "200",
-            "metadata": {},
+
             "error": ""
         }
     except Exception as ex:
@@ -64,10 +43,10 @@ async def delete_service(service_name):
         }
 
 
-@router.get("/odin/monitor/service/{service_name}", tags=["odin"])
+@router.get("/odin/service/{service_name}", tags=["odin"])
 async def get_status(service_name):
     try:
-        status = Helm.getServiceStatus(service_name)
+        status = Utils.getJson(Helm.getServiceStatus(service_name))
         return {
             "status": "200",
             "metadata": status,
@@ -84,7 +63,7 @@ async def get_status(service_name):
 @router.get("/odin/services/", tags=["odin"])
 async def get_all_services():
     try:
-        service_list = Helm.listAllServices()
+        service_list = Utils.getJson(Helm.listAllServices())
         return {
             "status": "200",
             "metadata": service_list,
@@ -96,4 +75,3 @@ async def get_all_services():
             "metadata": {},
             "error": "Failed to fetch all services: " + str(ex)
         }
-
