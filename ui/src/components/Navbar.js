7140a7918d31f3af33bfbcd23f9e54ce665240b1
anuragsarkar97 <sarkar.anurag@outlook.com>
2021-06-13 11:37:52 +0530

    added google logout

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -3,30 +3,27 @@ import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {useHistory} from "react-router";
 import {auth, googleProvider} from "../config/firebase";
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 export default function NavbarComponent() {
 
-    const history = useHistory();
-
     useEffect(async () => {
-        // await sleep(45000)
-        if (localStorage.getItem("expireAt") > Date.now() && localStorage.getItem("token")) return
+        if (parseInt(localStorage.getItem("expireAt")) > Date.now() && localStorage.getItem("token")) return
         await auth.signInWithPopup(googleProvider).then(result => {
-            debugger
             var credential = result.credential;
             var token = credential.accessToken;
             var user = result.user;
             localStorage.setItem("token", token)
             localStorage.setItem("user", JSON.stringify(user))
-            localStorage.setItem("expireAt", Date.now() + 10000)
+            localStorage.setItem("expireAt", Date.now() + 6000)
         })
     }, [])
 
     const logout = () => {
-        history.push("/login")
+        auth.signOut()
+        localStorage.removeItem("token")
+        localStorage.removeItem("user")
+        localStorage.removeItem("expireAt")
+        window.location.reload()
     }
     return (
         <div>
