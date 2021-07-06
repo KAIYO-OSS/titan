242d134e0be1ddb428d8bcbd1247d6cc313018f3
anuragsarkar97 <sarkar.anurag@outlook.com>
2021-07-06 09:36:07 +0530

    updated node version and access point

--- ui/server/index.js
+++ ui/server/index.js
@@ -2,13 +2,12 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const logger = require("./logger");
 const api = require("./api");
-const users = require("./users");
+const users = require("./api/users");
 const constants = require("./constants")
 const app = express();
 
-logger.info(path.join(__dirname, "..", "build"));
+console.log("UI server directory - " + path.join(__dirname, "..", "build"));
 
 users.createDefaultAdminUser().then(r => {
     console.log("admin created ")
@@ -17,19 +16,20 @@ users.createDefaultAdminUser().then(r => {
 })
 
 app
-    .use(cors)
-    .use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Expose-Headers", "x-access-token, Uid")
-        next();
-    })
-    .get("/health-check", (req, res) => res.send("OK"))
+    // .use(cors)
+    // .use(function (req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", "*");
+    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //     res.header("Access-Control-Expose-Headers", "x-access-token, Uid")
+    //     next();
+    // })
+    .get("/health", (req, res) => res.send("OK"))
     .use("/api", api)
-    .use("/users", users)
     .use(express.static(path.join(__dirname, "..", "build")))
     .use(bodyParser.json())
     .get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "..", "build/index.html"));
     })
     .listen(constants.PORT, () => console.log(`Server started http://localhost:${constants.PORT}`));
+
+app.timeout = 2000;
