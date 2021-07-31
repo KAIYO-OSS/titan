02fc1496decfd7150b6c80de263e93fdcc70d053
TheAlgo <dhirajjain0@gmail.com>
2021-07-31 16:48:54 +0530

    Remove acl_token from odin

--- api/routers/odin.py
+++ api/routers/odin.py
@@ -18,7 +18,7 @@ async def deploy_service(deploy_request: DeployRequest):
         output = Utils.getJson(Helm.deployService(deploy_request.service_name,
                                                   deploy_request.chart_name, deploy_request.values))
         service_list = Utils.getJson(Helm.listAllServices())
-        etcd.put('{acl_token}:service_list'.format(acl_token=deploy_request.acl_token), json.dumps(service_list))
+        etcd.put('service_list', json.dumps(service_list))
         return {
             "status": "200",
             "metadata": output,
@@ -28,12 +28,12 @@ async def deploy_service(deploy_request: DeployRequest):
         raise HTTPException(status_code=500, detail="Service deployment failed: " + str(ex))
 
 
-@router.delete("/odin/service/{service_name}/{acl_token}", tags=["odin"])
-async def delete_service(service_name, acl_token):
+@router.delete("/odin/service/{service_name}", tags=["odin"])
+async def delete_service(service_name):
     try:
         Helm.deleteService(service_name)
         service_list = Utils.getJson(Helm.listAllServices())
-        etcd.put('{acl_token}:service_list'.format(acl_token=acl_token), json.dumps(service_list))
+        etcd.put('service_list', json.dumps(service_list))
         return {
             "status": "200",
 
@@ -89,9 +89,9 @@ async def rollback_service(rollback_request: RollbackRequest):
 
 
 @router.get("/odin/services/", tags=["odin"])
-async def get_all_services(acl_token: str):
+async def get_all_services():
     try:
-        service_list = Utils.getJsonValue(etcd, '{acl_token}:service_list'.format(acl_token=acl_token))
+        service_list = Utils.getJsonValue(etcd, 'service_list')
         return {
             "status": "200",
             "metadata": service_list,
